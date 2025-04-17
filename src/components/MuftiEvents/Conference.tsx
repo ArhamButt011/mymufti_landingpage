5a3265ff4e12706a077275dd3be948c7ba9b9fd6
Myra Majid <lilzoshop.official@gmail.com>
2025-04-17 13:42:12 +0500

    Conference

---
+++ src/components/MuftiEvents/Conference.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Conference: React.FC = () => {
+    return (
+        <div>
+            {/* Conference component */}
+        </div>
+    );
+};
+
+export default Conference;
